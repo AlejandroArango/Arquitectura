e24dd53ca1d113fdb9586e2fe6422cd91b8b7e26
diegofdo3 <37670844+diegofdo3@users.noreply.github.com>
2018-04-13 17:24:46 -0500

    Add files via upload

---
+++ procesador1/PC_test.vhd
@@ -0,0 +1,103 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   17:07:52 04/13/2018
+-- Design Name:   
+-- Module Name:   C:/Users/utp/Xilinx/SysGen/14.7/datapath/PC_test.vhd
+-- Project Name:  datapath
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: pc
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY PC_test IS
+END PC_test;
+ 
+ARCHITECTURE behavior OF PC_test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT pc
+    PORT(
+         pcadder : IN  std_logic_vector(31 downto 0);
+         rst : IN  std_logic;
+         clk : IN  std_logic;
+         pcout : OUT  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal pcadder : std_logic_vector(31 downto 0) := (others => '0');
+   signal rst : std_logic := '0';
+   signal clk : std_logic := '0';
+
+ 	--Outputs
+   signal pcout : std_logic_vector(31 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: pc PORT MAP (
+          pcadder => pcadder,
+          rst => rst,
+          clk => clk,
+          pcout => pcout
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      pcadder<="00000000000000000000000000000000";
+      wait for 100 ns;	
+		pcadder<="00000000000000000000000000000100";
+      wait for 100 ns;	
+		pcadder<="00000000000000000000000000001000";
+      wait for 100 ns;	
+		pcadder<="00000000000000000000000000001100";
+      wait for 100 ns;	
+		pcadder<="00000000000000000000000000010000";
+      wait for 100 ns;	
+		rst<='1';
+		
+      wait for clk_period*10;
+
+      wait;
+   end process;
+
+END;
